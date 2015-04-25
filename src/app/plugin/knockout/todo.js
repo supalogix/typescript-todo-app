d5cc759e5cfff3b47782866e22990cde1b0326b3
Jonathan Nacionales <jnaciona@gmail.com>
2015-04-24 21:34:01 -0700

    Add knockout plugin POC
    I created a set of classes that can power a UI in a framework agnostic
    way. I use knockout as an example of how to implement these classes with
    a particular implementation.
    
    I still have to do a lot of work to complete it. However, I have
    implemented enough of the functionality to justify a commit.

---
+++ src/app/plugin/knockout/todo.js
@@ -0,0 +1,238 @@
+var ModelEvent = (function () {
+    function ModelEvent() {
+    }
+    /**
+     * Make an Abstract Method
+     *
+     * Typescript does not have abstract methods. Throw an error if a subclass
+     * tries to execute this function. This will inform the developer to override
+     * the function in a subclass.
+     */
+    ModelEvent.getName = function () {
+        throw Error("override me");
+    };
+    return ModelEvent;
+})();
+/// <reference path="ModelEvent.ts"/>
+var Delegate = (function () {
+    function Delegate() {
+    }
+    /**
+     * Make an Abstract Method
+     *
+     * Typescript does not have abstract methods. If a subclass does not
+     * override this method then it will throw an error. This will force the
+     * implementor to override this function.
+     */
+    Delegate.prototype.execute = function (ev) {
+        throw Error("override me");
+    };
+    return Delegate;
+})();
+/// <reference path="Delegate.ts" />
+/// <reference path="ModelEvent.ts" />
+var EventDispatcher = (function () {
+    function EventDispatcher() {
+        this.delegates = {};
+    }
+    /**
+     * Add a Delegate
+     *
+     * If a module cares about certain events then they can define a delegate for
+     * particular events. Use this function to make this class execute the
+     * delegate when someone uses this class to publish the event
+     *
+     * @param eventName the unique identifier you want to associate with a
+     * 	delegate
+     * @param delegate the delegate that you want this class to execute when
+     * 	someone uses this class to publish an event
+     */
+    EventDispatcher.prototype.addDelegate = function (eventName, delegate) {
+        this.delegates[eventName] = delegate;
+    };
+    /**
+     * Publish an Event
+     *
+     * A caller will use this function to publish an event.
+     *
+     * @param eventName the unique identifier that you want to associate with the
+     * 	event you want to publish
+     * @param ev the event that you want to publish.
+     */
+    EventDispatcher.prototype.publish = function (eventName, ev) {
+        var delegate = this.delegates[eventName];
+        delegate.execute(ev);
+    };
+    return EventDispatcher;
+})();
+var Guid = (function () {
+    function Guid() {
+    }
+    /**
+     * Generate a GUID
+     *
+     * Use this function to generate a guid.
+     *
+     * @returns a guid
+     */
+    Guid.create = function () {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    };
+    return Guid;
+})();
+/// <reference path="../../common/Guid.ts"/>
+var Item = (function () {
+    function Item(name) {
+        this.name = name;
+        this.guid = Guid.create();
+    }
+    return Item;
+})();
+/// <reference path="../../../common/ModelEvent.ts"/>
+/// <reference path="../../domain/Item.ts"/>
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var ItemAddedEvent = (function (_super) {
+    __extends(ItemAddedEvent, _super);
+    function ItemAddedEvent(item) {
+        _super.call(this);
+        this.item = item;
+    }
+    ItemAddedEvent.getName = function () {
+        return "ItemAddedEvent";
+    };
+    return ItemAddedEvent;
+})(ModelEvent);
+/// <reference path="../../../common/ModelEvent.ts"/>
+/// <reference path="../../domain/Item.ts"/>
+var ItemRemovedEvent = (function (_super) {
+    __extends(ItemRemovedEvent, _super);
+    function ItemRemovedEvent(item) {
+        _super.call(this);
+        this.item = item;
+    }
+    ItemRemovedEvent.getName = function () {
+        return "ItemRemovedEvent";
+    };
+    return ItemRemovedEvent;
+})(ModelEvent);
+/// <reference path="../../common/EventDispatcher.ts"/>
+/// <reference path="../domain/Item.ts"/>
+/// <reference path="../notification/item_added/ItemAddedEvent.ts"/>
+/// <reference path="../notification/item_removed/ItemRemovedEvent.ts"/>
+var TodoListModel = (function () {
+    function TodoListModel(delegator) {
+        this.delegator = delegator;
+        this.items = [];
+    }
+    TodoListModel.prototype.addItem = function (item) {
+        this.items.push(item);
+        this.delegator.publish(ItemAddedEvent.getName(), new ItemAddedEvent(item));
+    };
+    TodoListModel.prototype.removeItem = function (item) {
+        var index = this.items.indexOf(item);
+        if (index > -1) {
+            this.items.splice(index, 1);
+            this.delegator.publish(ItemRemovedEvent.getName(), new ItemRemovedEvent(item));
+        }
+    };
+    TodoListModel.prototype.getSize = function () {
+        return this.items.length;
+    };
+    return TodoListModel;
+})();
+/// <reference path="../../domain/Item.ts" />
+/// <reference path="ItemAddedEvent.ts" />
+/// <reference path="ItemAddedCallback.ts" />
+/// <reference path="../../../common/Delegate.ts" />
+var ItemAddedDelegate = (function (_super) {
+    __extends(ItemAddedDelegate, _super);
+    function ItemAddedDelegate(delegate) {
+        _super.call(this);
+        this.delegate = delegate;
+    }
+    ItemAddedDelegate.prototype.execute = function (ev) {
+        var item = ev.item;
+        this.delegate(item);
+    };
+    return ItemAddedDelegate;
+})(Delegate);
+/// <reference path="../../domain/Item.ts" />
+/// <reference path="ItemRemovedCallback.ts" />
+/// <reference path="ItemRemovedEvent.ts" />
+/// <reference path="../../../common/Delegate.ts" />
+var ItemRemovedDelegate = (function (_super) {
+    __extends(ItemRemovedDelegate, _super);
+    function ItemRemovedDelegate(delegate) {
+        _super.call(this);
+        this.delegate = delegate;
+    }
+    ItemRemovedDelegate.prototype.execute = function (ev) {
+        var item = ev.item;
+        this.delegate(item);
+    };
+    return ItemRemovedDelegate;
+})(Delegate);
+/// <reference path="../../model/TodoListModel.ts"/>
+/// <reference path="../../domain/Item.ts"/>
+var AddItemCommand = (function () {
+    function AddItemCommand(model) {
+        this.model = model;
+    }
+    AddItemCommand.prototype.execute = function (item) {
+        this.model.addItem(item);
+    };
+    return AddItemCommand;
+})();
+/// <reference path="../../model/TodoListModel.ts"/>
+/// <reference path="../../domain/Item.ts"/>
+var RemoveItemCommand = (function () {
+    function RemoveItemCommand(model) {
+        this.model = model;
+    }
+    RemoveItemCommand.prototype.execute = function (item) {
+        this.model.removeItem(item);
+    };
+    return RemoveItemCommand;
+})();
+/// <reference path="model/TodoListModel.ts" />
+/// <reference path="../common/EventDispatcher.ts" />
+/// <reference path="notification/item_added/ItemAddedCallback.ts" />
+/// <reference path="notification/item_added/ItemAddedEvent.ts" />
+/// <reference path="notification/item_added/ItemAddedDelegate.ts" />
+/// <reference path="notification/item_removed/ItemRemovedCallback.ts" />
+/// <reference path="notification/item_removed/ItemRemovedDelegate.ts" />
+/// <reference path="notification/item_removed/ItemRemovedEvent.ts" />
+/// <reference path="action/add_item/AddItemCommand.ts" />
+/// <reference path="action/remove_item/RemoveItemCommand.ts" />
+var TodoListFacade = (function () {
+    function TodoListFacade() {
+        this.delegator = new EventDispatcher();
+        this.model = new TodoListModel(this.delegator);
+    }
+    TodoListFacade.prototype.registerItemAddedCallback = function (callback) {
+        this.delegator.addDelegate(ItemAddedEvent.getName(), new ItemAddedDelegate(callback));
+    };
+    TodoListFacade.prototype.registerItemRemovedCallback = function (callback) {
+        this.delegator.addDelegate(ItemRemovedEvent.getName(), new ItemRemovedDelegate(callback));
+    };
+    TodoListFacade.prototype.getAddItemCommand = function () {
+        var command = new AddItemCommand(this.model);
+        return command;
+    };
+    TodoListFacade.prototype.getRemoveItemCommand = function () {
+        var command = new RemoveItemCommand(this.model);
+        return command;
+    };
+    TodoListFacade.prototype.getModel = function () {
+        return this.model;
+    };
+    return TodoListFacade;
+})();
